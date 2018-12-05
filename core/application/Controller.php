870ce034f9f0b46836b2694d4a0325660c334525
DESKTOP-NFD142A\emdas <emdas93@gmail.com>
2018-12-06 01:27:25 +0900

    Simple controller implementation

--- core/application/Controller.php
+++ core/application/Controller.php
@@ -1,11 +1,29 @@
 <?php
 
 class Controller {
-    public function __construct(){
-        foreach ($_SERVER as $key => $value){
-            echo '$_SERVER'.'['.$key.'] => '.$value.'<BR>';
-        }
+    public $controller;
+    public $action;
 
-        echo $_SERVER['REQUEST_URI'];
+    public function __construct(){
+        // URL 자르기
+        $url = $_GET['url'];   
+        $urls = explode('/', $url);
+        
+        // Controller 지정
+        $this->controller = $urls[0];
+        
+        // Action 지정
+        $this->action = $url[1];
+        
+        // 파일 불러오기 용 컨트롤러 이름
+        $controller = ucwords($this->controller).'Controller';
+
+        // Controller 파일 불러오기
+        require './src/controller/'.$controller.'.php';
+
+        // 불러온 Controller 인스턴스 생성
+        $instance = new $controller;
+        var_dump($instance);
+        
     }
 }
